ad9ecf8a171ad4825a005d2d14f726396ca663b5
fredericopradel-byte <fredericopradel@hotmail.com>
2026-09-17 10:06:08 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'iv-planner-v12-ios-status-bar-20260917';
+const CACHE = 'iv-planner-v13-papi-20260917';
 const OWNED_CACHE_PREFIXES = ['iv-planner-', 'geiv-dvor-vor-'];
 
 // Todas as telas e dados locais necessários às funções primárias do app.
