21731aa2ce600a2a4f304ebf339984427c52c8b5
fredericopradel-byte <fredericopradel@hotmail.com>
2026-09-17 17:36:29 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'iv-planner-v23-gerenciador-pacotes-20260917';
+const CACHE = 'iv-planner-v24-aixm-brasil-20260917';
 const OWNED_CACHE_PREFIXES = ['iv-planner-', 'geiv-dvor-vor-'];
 const PACKAGE_DB = 'ivplanner-package-manager-v1';
 const PACKAGE_DB_VERSION = 1;
