0b2a144048bfffdcd8e124a3481da051b06d71f4
fredericopradel-byte <fredericopradel@hotmail.com>
2026-09-17 09:06:57 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'iv-planner-v10-legacy-defaults-20260917';
+const CACHE = 'iv-planner-v11-legacy-wind-label-20260917';
 const OWNED_CACHE_PREFIXES = ['iv-planner-', 'geiv-dvor-vor-'];
 
 // Todas as telas e dados locais necessários às funções primárias do app.
