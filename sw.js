41e843379978fe8b6fb78513ede5be4a022426a5
fredericopradel-byte <fredericopradel@hotmail.com>
2026-09-17 10:48:07 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'iv-planner-v13-papi-20260917';
+const CACHE = 'iv-planner-v14-papi-map-controls-20260917';
 const OWNED_CACHE_PREFIXES = ['iv-planner-', 'geiv-dvor-vor-'];
 
 // Todas as telas e dados locais necessários às funções primárias do app.
