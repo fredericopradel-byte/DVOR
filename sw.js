eeebd2100c24940777c24454bbbe11f6bc94555c
fredericopradel-byte <fredericopradel@hotmail.com>
2026-09-22 21:29:04 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'iv-planner-v43-backup-20260923';
+const CACHE = 'iv-planner-v44-dvor-scroll-legacy-fade-20260923';
 const OWNED_CACHE_PREFIXES = ['iv-planner-', 'geiv-dvor-vor-'];
 const PACKAGE_DB = 'ivplanner-package-manager-v1';
 const PACKAGE_DB_VERSION = 1;
